3b5ebef009885eefeceb5685669cb956814c7820
Ricardo Leon <ricardo.leon@ar.ey.com>
2019-09-08 16:01:39 -0300

    Implement Kernel SVM

---
+++ Part 3 - Classification/Section 17 - Kernel SVM/utils.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  8 00:31:07 2019
+
+@author: Ricardo León
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_classifier(X, y, plot_title, classifier):
+    from matplotlib.colors import ListedColormap
+    X_set, y_set = X, y
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                         np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                 alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                    c = ListedColormap(('red', 'green'))(i), label = j)
+    plt.title(plot_title)
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.show()
